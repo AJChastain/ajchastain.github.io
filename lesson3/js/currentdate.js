13877276622c688d165587ecf6fc45573cfd8b41
ajchastain <ajchastain@byui.edu>
2021-01-22 14:58:50 -0800

    helllp

---
+++ lesson3/js/currentdate.js
@@ -0,0 +1,6 @@
+let d = new Date();
+let year = d.getFullYear();
+document.getElementById("currentyear").innerHTML = year;
+
+let modified = new Date(document.lastModified);
+document.getElementById("lastModif").innerHTML = modified;
