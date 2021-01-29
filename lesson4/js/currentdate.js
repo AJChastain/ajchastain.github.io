02e996d6d0909063cc12f53425bc8a621de5180b
ajchastain <ajchastain@byui.edu>
2021-01-28 21:07:42 -0800

    oof

--- lesson4/js/currentdate.js
+++ lesson4/js/currentdate.js
@@ -1,7 +1,3 @@
-let d = new Date();
-let year = d.getFullYear();
-document.getElementById("currentyear").innerHTML = year;
-
 let modified = new Date(document.lastModified);
 document.getElementById("lastModif").innerHTML = modified;
 
