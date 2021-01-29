b60ba36ee2ce9518cb6dcfd33914c248a54b5b43
ajchastain <ajchastain@byui.edu>
2021-01-28 21:27:35 -0800

    dateee

--- lesson4/js/currentdate.js
+++ lesson4/js/currentdate.js
@@ -1,6 +1,11 @@
 let modified = new Date(document.lastModified);
 document.getElementById("lastModif").innerHTML = modified;
 
+
+
+
+
+
 function toggleMenu() {
     document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
 }
