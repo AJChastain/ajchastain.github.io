9e9ce57959666d483ee46ae615e39aed9110e163
ajchastain <ajchastain@byui.edu>
2021-01-29 16:04:32 -0800

    pushhh

--- lesson4/js/currentdate.js
+++ lesson4/js/currentdate.js
@@ -1,11 +1,11 @@
-let modified = new Date(document.lastModified);
-document.getElementById("lastModif").innerHTML = modified;
+//const modified = new Date();
 
+const options = {weekday: "long", day: "numeric", month: "long", year: "numeric"};
 
-
+document.getElementById("currentdate").innerHTML = new Date().toLocaleDateString("en-us", options);
 
 
 
 function toggleMenu() {
     document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
-}
+} 
