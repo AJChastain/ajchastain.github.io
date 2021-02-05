b0292cd3a6eedb7ea3a8dbae4ae4df709847e7c3
ajchastain <ajchastain@byui.edu>
2021-02-05 13:48:07 -0800

    1347

--- lesson5/js/currentdate.js
+++ lesson5/js/currentdate.js
@@ -9,3 +9,9 @@ document.getElementById("currentdate").innerHTML = new Date().toLocaleDateString
 function toggleMenu() {
     document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
 } 
+
+function satBanner () {
+    if weekday == "Friday" {
+        display: "Saturday = Preston Pancakes in the Park! 9:00A.M. Saturday at the city park pavilion."
+
+}}
