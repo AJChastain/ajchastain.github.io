47eaa6b78ba2a2fb125c67e5f38993f616a7d774
ajchastain <ajchastain@byui.edu>
2021-02-12 21:02:30 -0800

    thank the Lord!

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,18 +1,19 @@
-
 function getChilled() {
-    var temp = parsefloat(document.getElementById("temperature").innerHTML);
-    var ws = parsefloat(document.getElementById("wind_speed").innerHTML);
+    var temp = parseFloat(document.getElementById("temperature").value);
+    var ws = parseFloat(document.getElementById("wind_speed").value);
     var chill = calcChill(temp, ws);
-    chill = document.getElementById("wind_chill").innerHTML;
+    document.getElementById("wind_chill").innerHTML = chill;
+   
  } 
 
 function calcChill(temp, ws) {
     if (temp < 51 && ws > 3) {
-        let chilled = 35.74 + (0.6215 * temp) - (35.75 * Math.pow(ws, 0.16)) + (0.4275 * temp) * Math.pow(ws, 0.16);
+        var chilled = 35.74 + (0.6215 * temp) - (35.75 * Math.pow(ws, 0.16)) + (0.4275 * temp) * Math.pow(ws, 0.16);
         return chilled;
     }
     else {
-        chilled = "N/A";
+        chilled = "(N/A)";
         return chilled;
     }
 }
+
