6ed43692f5c63a87da417131b6a95d2f24d15cca
ajchastain <ajchastain@byui.edu>
2021-02-12 20:05:18 -0800

    help

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -7,7 +7,7 @@ function getChilled() {
  } 
 
 function calcChill(temp, ws) {
-    if (temp <= 50 && ws > 3) {
+    if (temp < 51 && ws > 3) {
         let chilled = 35.74 + (0.6215 * temp) - (35.75 * Math.pow(ws, 0.16)) + (0.4275 * temp) * Math.pow(ws, 0.16);
         return chilled;
     }
