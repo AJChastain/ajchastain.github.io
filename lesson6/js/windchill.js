6b3f5fb3038bc391ab6a05cd40f20c88d4743b1d
ajchastain <ajchastain@byui.edu>
2021-02-12 21:23:29 -0800

    SAVED

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,6 +1,6 @@
 function getChilled() {
-    var temp = parseFloat(document.getElementById("temperature").value);
-    var ws = parseFloat(document.getElementById("wind_speed").value);
+    var temp = parseFloat(document.getElementById("temperature").innerHTML);
+    var ws = parseFloat(document.getElementById("wind_speed").innerHTML);
     var chill = calcChill(temp, ws);
     document.getElementById("wind_chill").innerHTML = chill;
    
@@ -9,13 +9,13 @@ function getChilled() {
 function calcChill(temp, ws) {
     if ((temp < 51) && (ws > 3)) {
         var exp = Math.pow(ws, 0.16);
-        var chilled = 35.74 + (0.6215 * temp) - (35.75 * exp) + (0.4275 * temp) * exp;
-        return chilled;
+        var chilled = 35.74 + 0.6215 * temp - 35.75 * exp + 0.4275 * temp * exp;
+        chilled = chilled.toFixed(0);
     }
     else {
         chilled = "(N/A)";
-        return chilled;
+        
     }
-   
+   return chilled;
 }
 
