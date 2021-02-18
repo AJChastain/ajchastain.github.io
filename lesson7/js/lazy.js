c79007660e2a18f02bd4f78d3a555d7ee20b309f
ajchastain <ajchastain@byui.edu>
2021-02-17 20:50:06 -0800

    gdkf;jgav; rlkg

--- lesson7/js/lazy.js
+++ lesson7/js/lazy.js
@@ -10,7 +10,7 @@ function preloadImage(img) {
 
 const imgOptions = {
     threshold: 1,
-    rootMargin: "0px 0px 300px 0px"
+    rootMargin: "0px 0px 400px 0px"
 };
 
 const imgObserver = new IntersectionObserver((entries, imgObserver) => {
