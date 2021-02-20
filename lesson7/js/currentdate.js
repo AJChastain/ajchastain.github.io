c9e0a9936d106d115fda97fd5b67331c20e5559b
ajchastain <ajchastain@byui.edu>
2021-02-20 10:52:51 -0800

    saved

--- lesson7/js/currentdate.js
+++ lesson7/js/currentdate.js
@@ -17,3 +17,35 @@ function toggleMenu() {
     if (Day == 5) {
       aside.style.display = (aside.style.display == "block") ? "none" : "block";
 }
+
+const images = document.querySelectorAll("[data-src]");
+
+function preloadImage(img) {
+    const src = img.getAttribute("data-src");
+    if(!src) {
+        return;
+    }
+    img.src = src;
+    img.onload = () => {img.removeAttribute("data-src")};
+}
+
+const imgOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px 300px 0px"
+};
+
+const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+    entries.forEach(entry => {
+        if(!entry.isIntersecting) {
+            return;
+        }
+        else {
+            preloadImage(entry.target);
+            imgObserver.unobserve(entry.target);
+        }
+    });
+}, imgOptions);
+
+images.forEach(image => {
+    imgObserver.observe(image);
+});
