46b6f98e6877016601b76f1ada2ae6de55b3058d
ajchastain <ajchastain@byui.edu>
2021-03-05 16:02:46 -0800

    please help

--- lesson9/js/currentdate.js
+++ lesson9/js/currentdate.js
@@ -61,7 +61,7 @@ function adjustRating(rating) {
 const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
 
 fetch(requestURL)
-  .then(function (response) {
+    .then(function (response) {
     return response.json();
   })
   .then(function (jsonObject) {
@@ -76,27 +76,27 @@ let h2 = document.createElement('h2');
     h2.textContent = towns[i].name;
     card.appendChild(h2);
 
-let motto = document.createElement('motto');
+let motto = document.createElement("motto");
     motto.textContent = towns[i].motto;
     card.appendChild(motto);
 
-let yearFounded = document.createElement('yearFounded');
+let yearFounded = document.createElement("yearFounded");
     yearFounded.textContent = "Year Founded: " + towns[i].yearFounded;
     card.appendChild(yearFounded);
 
-let pop = document.createElement('currentPopulation');
+let pop = document.createElement("currentPopulation");
     pop.textContent = "Population: " + towns[i].currentPopulation;
     card.appendChild(pop);
 
-let rain = document.createElement('averageRainfall');
+let rain = document.createElement("averageRainfall");
     rain.textContent = "Annual Rainfall: " + towns[i].averageRainfall;
     card.appendChild(rain);
 
-let image = document.createElement('img');
+let image = document.createElement("img");
     image.setAttribute("src", towns[i].photo);
     card.appendChild(image); 
 
-let alt = document.createElement('alt');
+let alt = document.createElement("alt");
     alt.setAttribute("alt", towns[i].name + ", " + "Idaho");
     card.appendChild(alt);
 
