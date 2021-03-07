506a71bfbcd46202ad92fa1449c85c7995bab481
ajchastain <ajchastain@byui.edu>
2021-03-06 20:27:43 -0800

    ndg md,b

--- lesson9/js/json.js
+++ lesson9/js/json.js
@@ -27,7 +27,7 @@ home.forEach(town => {
     cards.append(card);
 
     let motto = document.createElement('motto');
-    motto.innerHTML = `${town.motto}` ;
+    motto.innerHTML = `${town.motto}`;
     card.append(motto);
     cards.append(card);
 
@@ -47,63 +47,33 @@ home.forEach(town => {
     cards.append(card);
 
     let image = document.createElement("img");
-    image.setAttribute("src", `images/towns/${town.image}`);
+    image.setAttribute("src", `images/${town.image}`);
     image.setAttribute("alt", town.name + " photo");
-   /* image.setAttribute = `${images/FishHaven.jpg}`;
-    image.setAttribute = `${images/SodaSprings.jpg}`;*/
   
+/*//credit
+let selector = "";
+switch (town.name) {
+  case "Preston":
+    selector = "preston";
+    break;
+  case "Soda Springs":
+    selector = "sodaSprings";
+    break;
+  case "Fish Haven":
+    selector = "fishHaven";
+    break;
+}
+townSection = document.querySelector(`section#${selector}`);
+
+      townSection.appendChild(image);*/
 
     card.append(image);
     cards.append(card);
 
-    
-    
-
-
-   
-
+      
 });
 
-/*for (let i = 1; i < towns.length; i++ )    
-{
 
-
-let card = document.createElement('section');
-
-let h2 = document.createElement('h2');
-    h2.textContent = towns[i].name;
-    card.appendChild(h2);
-
-let motto = document.createElement("motto");
-    motto.textContent = towns[i].motto;
-    card.appendChild(motto);
-
-let yearFounded = document.createElement("yearFounded");
-    yearFounded.textContent = "Year Founded: " + towns[i].yearFounded;
-    card.appendChild(yearFounded);
-
-let pop = document.createElement("currentPopulation");
-    pop.textContent = "Population: " + towns[i].currentPopulation;
-    card.appendChild(pop);
-
-let rain = document.createElement("averageRainfall");
-    rain.textContent = "Annual Rainfall: " + towns[i].averageRainfall;
-    card.appendChild(rain);
-
-let image = document.createElement("img");
-    image.setAttribute("src", towns[i].photo);
-        
-    card.appendChild(image); 
-
-let alt = document.createElement("alt");
-    alt.setAttribute("alt", towns[i].name + ", " + "Idaho");
-    card.appendChild(alt);
-
-    document.querySelector('div.cards').appendChild(card);
-   
-};
-  
-*/
 
   });
 
