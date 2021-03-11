1e9efba52a153dec8dd4eccc14e1db8b80e6ce69
ajchastain <ajchastain@byui.edu>
2021-03-10 20:55:03 -0800

    acb

---
+++ lesson10/js/weatherapi.js
@@ -0,0 +1,15 @@
+const apiURL="https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=cc9663bac24d48ffba1cbf202d93ed9f&units=imperial";
+fetch(apiURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        console.log(jsObject);
+        document.getElementById('current-temp').textContent = jsObject.main.temp; 
+       
+        const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
+        const desc = jsObject.weather[0].description;  // note how we reference the weather array
+        document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
+        document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
+        document.getElementById('icon').setAttribute('alt', desc);
+   
+   
+    });
