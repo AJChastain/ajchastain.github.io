d2bca32429483eef38f8835577e329ae62390e17
ajchastain <ajchastain@byui.edu>
2021-03-17 17:44:02 -0700

    YESSSS

--- lesson11/js/events.js
+++ lesson11/js/events.js
@@ -9,34 +9,15 @@ fetch(requestURL)
 
 const towns = jsonObject['towns'];
 
-// Code inspired by Jeremiah Orcutt, who rocks at this.
-/*let a = 0;
-for (a = 0; a < towns.length; a++) {
-  let town = towns[a].name;
-  if (town == "Preston") {
-    let event = towns[a].events;
-    document.getElementById("cards2").textContent = event;
-  }
 
-  else if (town == "Fish Haven") {
-    let event = towns[a].events;
-    document.getElementById("cards3").textContent = event;
-  }
-
-  else if (town == "Soda Springs") {
-    let event = towns[a].events;
-    document.getElementById("cards4").textContent = event;
-  }
-}
-*/
-//let town = towns[a].name; 
+//Preston
 const home = [
-  towns.find((town) => town.name == "Fish Haven"),
-  towns.find((town) => town.name == "Preston"),
-  towns.find((town) => town.name == "Soda Springs"),];
+towns.find((town) => town.name == "Preston"),
+//towns.find((town) => town.name == "Fish Haven"),
+//towns.find((town) => town.name == "Soda Springs"),
+];
 
 const cards = document.querySelector('.cards2');
-
 home.forEach(town => {
   
   let card = document.createElement('section'); 
@@ -46,8 +27,13 @@ home.forEach(town => {
   cards.append(card);
 
   let events = document.createElement("events");
-  events.innerHTML = `${town.events}`;
+  events.innerHTML = town.events[0] + "<br>" +
+    town.events[1] + "<br>" + town.events[2] + "<br>";
   card.append(events);
   cards.append(card);
 });
+
+
+
+
   });
---
+++ lesson11/js/eventsfish.js
@@ -0,0 +1,33 @@
+const requestURL ='https://byui-cit230.github.io/weather/data/towndata.json';
+
+fetch(requestURL)
+    .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+  console.table(jsonObject);
+
+const towns = jsonObject['towns'];
+const home3 = [
+towns.find((town) => town.name == "Fish Haven"),
+//towns.find((town) => town.name == "Preston"),
+//towns.find((town) => town.name == "Soda Springs")
+];
+  
+  const cards = document.querySelector('.cards4');
+  
+  home3.forEach(town => {
+    
+    let card = document.createElement('section'); 
+    let h2 = document.createElement('h2');
+    h2.innerHTML = "Upcoming Events" ;
+    card.append(h2);
+    cards.append(card);
+  
+    let events = document.createElement("events");
+    events.innerHTML = town.events[0] + "<br>" +
+      town.events[1] + "<br>" + town.events[2] + "<br>";
+    card.append(events);
+    cards.append(card);
+  });
+});
