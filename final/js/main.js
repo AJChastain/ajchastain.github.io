b2574e5815e19acbe50fc4b563b6f61be1a3d60a
ajchastain <ameliajchastain@gmail.com>
2021-03-23 12:06:43 -0700

    save

--- final/js/main.js
+++ final/js/main.js
@@ -3,8 +3,8 @@ const options = {weekday: "long", day: "numeric", month: "long", year: "numeric"
 document.getElementById("currentdate").innerHTML = new Date().toLocaleDateString("en-us", options);
 
 //Toggle hamburger for main nav
-function toggleMenu() {
-    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+function toggleNav() {
+    document.getElementsByClassName("navbar")[0].classList.toggle("responsive");
 } 
 
 
