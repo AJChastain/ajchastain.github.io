51a77732ae6c63e0592d7b98f2859d3b07924fa0
ajchastain <ameliajchastain@gmail.com>
2021-04-04 15:16:53 -0700

    idk man

--- final/js/main.js
+++ final/js/main.js
@@ -10,6 +10,78 @@ function toggleNav() {
     document.getElementsByClassName("navigation")[0].classList.toggle("responsive");*/
 //} 
 
+//Directory
 
 
+/*const header = document.querySelector('header');
+const section = document.querySelector('section');
+
+let requestURL = "https://ajchastain.github.io/json/directory.json";
+let request = new XMLHttpRequest();  
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+request.onload = directme() {
+    const directory = request.response;
+    business(busName);
+    location(address, cityzip);
+    callme(phone);
+    webs(site);
+    pic(logo);
+}*/
+
+const requestURL ='https://ajchastain.github.io/final/json/directory.json';
+
+fetch(requestURL)
+    .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+ 
+
+const business = jsonObject['busName'];
+
+const cards = document.querySelector('.cards');
+
+let card = document.createElement('section');
     
+   
+    let h2 = document.createElement('h2');
+    h2.innerHTML = `${directory.busName}` ;
+    card.append(h2);
+    cards.append(card);
+
+    let address = document.createElement('address');
+    address.innerHTML = `${directory.address}`;
+    card.append(address);
+    cards.append(card);
+
+    let city = document.createElement("city");
+    city.innerHTML = "Founded in: " + `${directory.cityzip}` ;
+    card.append(cityd);
+    cards.append(card);
+
+    let currentPopulation = document.createElement("currentPopulation");
+    currentPopulation.innerHTML = "Population: " + `${town.currentPopulation}` ;
+    card.append(currentPopulation);
+    cards.append(card);
+
+    let averageRainfall = document.createElement("averageRainfall");
+    averageRainfall.innerHTML = "Annual rainfall: " + `${town.averageRainfall}` ;
+    card.append(averageRainfall);
+    cards.append(card);
+
+    let image = document.createElement("img");
+   
+    image.setAttribute("src", `images/${town.photo}`);
+    image.setAttribute("alt", town.name + " photo");
+  
+
+    card.append(image);
+    cards.append(card);
+      
+});
+
+
+  });
+
